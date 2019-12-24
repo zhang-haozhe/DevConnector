1e3cbd6a616294757a7f8bf8bb8f25d30f74aae7
Haozhe Zhang <zhz19980514@ufl.edu>
2019-12-23 19:56:21 -0500

    profile built

--- models/Profile.js
+++ models/Profile.js
@@ -56,5 +56,60 @@ const ProfileSchema = new mongoose.Schema({
         type: String
       }
     }
-  ]
+  ],
+
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
