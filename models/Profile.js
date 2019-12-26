e67a4cdcb4015204be9770a8b1fc5ed73be8080e
Haozhe Zhang <zhz19980514@ufl.edu>
2019-12-25 22:34:25 -0500

    create and update profile

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+//mongoose.set("useFindAndModify", false);
+
 const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
